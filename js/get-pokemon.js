5d7a4a757b8da4fbaf98c63cd157884a720f676a
tworns <tworns@yahoo.com>
2016-06-09 21:23:45 -0400

    Can now search pokemon by number

--- js/get-pokemon.js
+++ js/get-pokemon.js
@@ -17,7 +17,7 @@ function addPokemon(pokemon) {
     .attr('href', pokemon.url);
 
   li.attr('data-id', pokemon.id);
-
+li.attr('class', 'list-group-item');
   $('#pokemonList').append(li);
 }
 function sprite(data) {
@@ -38,7 +38,25 @@ function sprite(data) {
   $(this).replaceWith(replacementDiv);
   typeData(data, $(this).closest('li'));
 }
+function search(data){
+  var newDiv = $('<div style = "margin-left: auto; margin-right:auto; width: 50em;">');
+  var front = $('<img>');
+  var back = $('<img>');
+  var frontDef = $('<img>');
+  var backDef = $('<img>');
+  frontDef.attr('src',data.sprites.front_default);
+  backDef.attr('src',data.sprites.back_default);
+  front.attr('src', data.sprites.front_shiny);
+  back.attr('src',data.sprites.back_shiny);
+  newDiv.append(frontDef);
+  newDiv.append(backDef);
+  newDiv.append(front);
+  newDiv.append(back);
+  newDiv.append(typeData(data));
+  newDiv.attr('id', "pkmnDisplay");
+  $('#pkmnDisplay').replaceWith(newDiv);
 
+}
 function typeData(data){
   var type1 = data.types[0].type.name;
   var pkmnName = data.forms[0].name;
@@ -49,7 +67,15 @@ function typeData(data){
   }
   return typeString;
 }
-
+$('#pkmnSubmit').on('click', function(e){
+    e.preventDefault();
+    debugger;
+    var urlStr = 'https://pokeapi.co/api/v2/pokemon/' + $('#pkmnSearch').val();
+    $.get({
+      url: urlStr,
+      success: search.bind(this)
+    });
+});
 $(document).on('click', 'a',function(e){
     e.preventDefault();
 
